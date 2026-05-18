6a8aab4b07c6caa1d0643c1c9dccd5b2d5f5e094
Evgeni Chasnovski <evgeni.chasnovski@gmail.com>
2026-05-18 17:57:59 +0300

    test(extra): make testing `pickers.hl_groups()` more robust
    Details:
    - Screenshot contains information about the total number of highlight
      group. Its text was ignored, but not attribute. As the number of
      groups crossed 1000, highlighting range of that number also changed.

--- tests/test_extra.lua
+++ tests/test_extra.lua
@@ -2548,7 +2548,7 @@ T['pickers']['hl_groups()']['works'] = function()
   child.lua_notify('_G.return_item = MiniExtra.pickers.hl_groups()')
   validate_picker_name('Highlight groups')
   type_keys('^Spell')
-  child.expect_screenshot({ ignore_text = { 9 } })
+  child.expect_screenshot({ ignore_text = { 9 }, ignore_attr = { 9 } })
 
   -- Should use same group for line highlighting
   local matches = get_picker_matches().all
@@ -2561,7 +2561,7 @@ T['pickers']['hl_groups()']['works'] = function()
 
   -- Should have proper preview
   type_keys('<Tab>')
-  child.expect_screenshot({ ignore_text = { 9 } })
+  child.expect_screenshot({ ignore_text = { 9 }, ignore_attr = { 9 } })
 
   -- Should properly choose
   type_keys('<CR>')
